09783f7730099f3ff9e9486cef13648b8da889cc
Deador <62415631+Deador@users.noreply.github.com>
2022-07-09 13:04:38 +0300

    Add files via upload

---
+++ modules/cart/clearCart.js
@@ -0,0 +1,20 @@
+import clearChipCart from "/modules/cart/clearChipCartActive.js";
+export default clearCart;
+
+function clearCart() {
+  const clearCart = document.querySelector(".clear_cart");
+  const itemsContainer = document.querySelector(".items_container");
+  let count = document.querySelector(".count_product");
+
+  // Очистка всей корзины
+  clearCart.addEventListener("click", function () {
+    const itemCart = itemsContainer.querySelectorAll(".cart_item"); // Нашел все карточки
+
+    // Перебрал каждый элемент
+    for (let item of itemCart) {
+      item.remove(); // Удаляю каждый полученный элемент
+      count.innerText = 0; // Присваиваю чипу в корзине 0
+      clearChipCart(); // Запускаю функцию, которая скрывает чип в корзине
+    }
+  });
+}

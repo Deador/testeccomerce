bee9786eb5211a6be915f2ab0d667bf22e250f5d
Deador <62415631+Deador@users.noreply.github.com>
2022-07-10 17:01:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ---------------------Импорты------------------
-import clearChipCart from "modules/cart/clearChipCartActive.js";
-import itogCost from "modules/cart/itogCost.js";
-import deleteCartItem from "modules/cart/deleteCartItem.js";
-import clearCart from "modules/cart/clearCart.js";
+// import clearChipCart from "modules/cart/clearChipCartActive.js";
+// import itogCost from "modules/cart/itogCost.js";
+// import deleteCartItem from "modules/cart/deleteCartItem.js";
+// import clearCart from "modules/cart/clearCart.js";
 
 // ----------------------Элементы--------------------
 const itemsCart = document.querySelector(".items_container");
@@ -126,6 +126,6 @@ async function cartProduct() {
     };
   });
 }
-deleteCartItem(); // Удаление товара из корзины
-clearCart(); // Полная очистка корзины
+// deleteCartItem(); // Удаление товара из корзины
+// clearCart(); // Полная очистка корзины
 cartProduct();
